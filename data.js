1740bc812d119a1d8681a8c09668dd64be7f82be
shiningbeer <shiningbeer@163.com>
2018-12-17 23:06:01 +0800

    s

---
+++ data.js
@@ -0,0 +1,22 @@
+const aa = {
+    'name': 'kdkd',
+    'geoLocation': {
+        'latitude': 122,
+        'longtitude': 133,
+    },
+    'runningStatus': 'normal',//normal,abnormal
+    'city': '',
+    'state': '',
+    'Country': '',
+    'powerStage': 'generate',//generation,tranformation,distribution,consumption
+    'controledBy': '',//name of control center
+    'nature': 'civil',//civil, military,unknown
+    'capacity': 10000,//for g and c
+    'voltage': 1000,//for t and d
+    'type': '', //all kinds of types of g,t,d,c
+    'importance': 'critical',//critical,important, common
+
+
+
+
+}
